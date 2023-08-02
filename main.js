9709c7b3d8d02067aff6c06a8babb5d4dfa20002
Karangwa Ajika <52496651+karangwaajika@users.noreply.github.com>
2023-08-02 17:20:15 +0200

    feat: display single country's other information

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const displayCountries = document.querySelector('.display-countries');
 const card = document.querySelector('.display-countries .card');
 const countryFlag = document.querySelector('.card img');
-const countryDetails = document.querySelector('.content ul');
-const countryName = document.querySelector('.content h3');
+const populationValue = document.querySelector('.population .value');
+const regionValue = document.querySelector('.region .value');
+const capitalValue = document.querySelector('.capital .value');
+const countryName = document.querySelector('.content .header');
 const laoderDiv = document.querySelector('.loader-div');
 
 async function showCountries(){
@@ -12,10 +14,15 @@ async function showCountries(){
         mode: 'cors'
     };
     try {
+    
         const response = await fetch(url, options);
         const result = await response.json();
         countryFlag.src = result[6].flag; 
         countryName.textContent = result[6].name; 
+        populationValue.textContent = result[6].population;
+        regionValue.textContent = result[6].region;
+        capitalValue.textContent = result[6].capital;
+
         console.log(result[6]);
         displayCountries.style.display = "flex";
         laoderDiv.style.display="none";
