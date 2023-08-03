aeecf6566a2d0fb8804ccc14b2d9904fce1056ac
Karangwa Ajika <52496651+karangwaajika@users.noreply.github.com>
2023-08-03 11:14:19 +0200

    feat: convert population digits to comma format

--- main.js
+++ main.js
@@ -22,8 +22,38 @@ async function showCountries(){
         populationValue.textContent = result[6].population;
         regionValue.textContent = result[6].region;
         capitalValue.textContent = result[6].capital;
+        let listPopulationDigits = populationValue.textContent.split("");
+        let reversePopulationDigits = [];
+        
+        for(i in listPopulationDigits){
+            reversePopulationDigits.unshift(listPopulationDigits[i]); //reverse string so to start putting commas starting from right
+        }
+        let reversePopulationDigitsWithComma =  [...reversePopulationDigits];
+        for(let j=0; j < reversePopulationDigits.length; j++){
+            if((j+1)%3 == 0 && (j+1) != reversePopulationDigits.length){ // target every the 3rd digits and exclude the last 3rd one
+                console.log(reversePopulationDigits[j]);
+                const numberOfComma = reversePopulationDigitsWithComma
+                                .filter(element=>element == ',')
+                                .reduce((tot, commas)=>{
+                                    return tot+1;
+                                },0); 
+               
+                let commaStartingPosition = j+numberOfComma+1; // find the position to put the comma, by taking consideration of new incoming element (commas) as it updates the string length
+        
+                reversePopulationDigitsWithComma.splice(commaStartingPosition,0, ',');
+            } 
+        }
+        let listPopulationDigitsWithComma = [];
+        for(i in reversePopulationDigitsWithComma){
+            listPopulationDigitsWithComma.unshift(reversePopulationDigitsWithComma[i]);
+        }
+        const populationDigitsWithComma = listPopulationDigitsWithComma.reduce((text,element)=>{
+            return text+element;
+        },'')
 
+        
         console.log(result[6]);
+        populationValue.textContent = populationDigitsWithComma;
         displayCountries.style.display = "flex";
         laoderDiv.style.display="none";
         
